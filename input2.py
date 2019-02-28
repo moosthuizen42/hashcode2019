0dc997011feab6f8d91fc238297867e53584cf6b
Hirza-Tango <dslogrove@gmail.com>
2019-02-28 13:55:30 -0500

    a

--- input2.py
+++ input2.py
@@ -1,18 +1,18 @@
 def input(filename):
     all_tags = []
-    vert = []
-    hor = []
+    vert = {}
+    hor = {}
     f = open(filename).readlines()
     pics = []
-    for line in f[1:]:
+    for i, line in enumerate(f[1:]):
         stuff = line.split()
         o = stuff[0]
         for tag in stuff[2:]:
             if not tag in all_tags:
                 all_tags.append(tag)
         if (o == "H"):
-            hor.append(list(map(lambda x: all_tags.index(x), stuff[2:])))
+            hor[i] = list(map(lambda x: all_tags.index(x), stuff[2:]))
         else:
-            vert.append(list(map(lambda x: all_tags.index(x), stuff[2:])))
+            vert[i] = list(map(lambda x: all_tags.index(x), stuff[2:]))
     return(vert, hor)
 
