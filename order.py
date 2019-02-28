e33753348570a3c61e274debc5ca1ae6938e6a16
Michael <u13085248@tuks.co.za>
2019-02-28 21:46:12 +0200

    ghl2

--- order.py
+++ order.py
@@ -86,14 +86,14 @@ def doRandomRemove(current_slides, unused_slides_H, unused_slides_V):
         if (removed_slide.h_image is not None):
             unused_slides_H.append(removed_slide)
         else:
-            unused_slides_V.append(
+            unused_slides_V.append(Slide(
                 v_image_1 = removed_slide.v_image_1,
                 v_tags_1 = removed_slide.v_tags_1,
-            )
-            unused_slides_V.append(
+            ))
+            unused_slides_V.append(Slide(
                 v_image_1 = removed_slide.v_image_2,
                 v_tags_1 = removed_slide.v_tags_2,
-            )
+            ))
 
         del current_slides[remove_index]
 
