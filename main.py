6257cd29938d653a785f91891f6264fa008e0e3f
Jean van den Berg <jeanvdberg1994@gmail.com>
2019-02-28 21:32:51 +0200

    STUFF

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
 from score import getScore
+from input import read_input
+from slide import Slide
+from random import randint
+# from order import repeatRandomness
+from tqdm import tqdm
+import itertools
 
 def main():
-    node_a = {
-        "tags": ['test', 'cat', 'dog']
-    }
-    node_b = {
-        "tags": ['test', 'pizza', 'home', 'beer']
-    }
+    # read_input('inputs/b_lovely_landscapes.txt')
+    P, v, h = read_input('inputs/c_memorable_moments.txt')
+    # read_input('inputs/d_pet_pictures.txt')
+    # read_input('inputs/e_shiny_selfies.txt')
 
-    print(getScore(node_a, node_b))
+    v_count = len(v)
+    h_count = len(h)
+
+    slideshow = []
+    for i, _h in enumerate(itertools.islice(h, 0, int(h_count/10))):
+        slideshow.append(_h)
+        del h[i]
+
+    v_temp = v[:int(v_count/10)]
+    for _v, _v2 in zip(v_temp[0:: 2], v_temp[1:: 2]):
+        index = randint(1, len(slideshow))
+
+        slideshow.insert(index, Slide(v_tags_1=_v.v_tags_1, v_tags_2=_v2.v_tags_1, v_image_1=_v.v_image_1, v_image_2=_v2.v_image_1))
+
+    h = h[100:]
+    v = v[100:]
+
+    print(len(slideshow))
+    print(slideshow)
+
+    # for _ in tqdm(P * 5):
+    #     repeatRandomness(slideshow, h, v)
 
 if __name__ == '__main__':
     main()
--- score.py
+++ score.py
@@ -6,3 +6,10 @@ def getScore(node_a, node_b):
     unique_a = len(list(set(tags_a) - set(tags_b)))
     unique_b = len(list(set(tags_b) - set(tags_a)))
     return min(common_tags, unique_a, unique_b)
+
+def scoreArray(slides):
+    score = 0
+    slides_count = len(slides)
+    for i, s in enumerate(slides):
+        if i + 1 < slides_count:
+            score += getScore(s, slides[i + 1])
