f39aea88f55cca2639d96898df807bfd8f4d744b
Hirza-Tango <dslogrove@gmail.com>
2019-02-28 14:33:08 -0500

    a

--- score.py
+++ score.py
@@ -1,7 +1,7 @@
 
 def getScore(node_a, node_b):
-    tags_a = node_a['tags']
-    tags_b = node_b['tags']
+    tags_a = node_a.v_tags_1 + node_a.v_tags_2 + node_a.h_tags
+    tags_b = node_b.v_tags_1 + node_b.v_tags_2 + node_b.h_tags
     common_tags = len(set(tags_a + tags_b))
     unique_a = len(set(tags_a) - set(tags_b))
     unique_b = len(set(tags_b) - set(tags_a))
