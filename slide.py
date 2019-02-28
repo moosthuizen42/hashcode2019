1896c875535a365867d533afd08da65658ac1ab8
Jean van den Berg <jeanvdberg1994@gmail.com>
2019-02-28 20:30:18 +0200

    slide class

--- slide.py
+++ slide.py
@@ -1,7 +1,7 @@
-class slide ():
 
-    __init__ (self):
-        V_image_1;
-        V_image_2;
-        H_image;
-        
+class Slide():
+    def __init__ (self, tags, v_image_1=None, v_image_2=None, h_image=None):
+        self.v_image_1 = v_image_1
+        self.v_image_2 = v_image_2
+        self.h_image = h_image
+        self.tags = tags
