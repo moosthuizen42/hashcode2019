d64925d1c6d60bc55f508db90fab779822207d16
Jean van den Berg <jeanvdberg1994@gmail.com>
2019-02-28 20:36:00 +0200

    updated slide class

--- slide.py
+++ slide.py
@@ -1,7 +1,6 @@
 
 class Slide():
     def __init__ (self, tags, v_image_1=None, v_image_2=None, h_image=None):
-        self.v_image_1 = v_image_1
-        self.v_image_2 = v_image_2
-        self.h_image = h_image
         self.tags = tags
+        ids = [v_image_1, v_image_2, h_image]
+        self.ids = filter(None, ids)
